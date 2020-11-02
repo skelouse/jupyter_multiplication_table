21d42b930ea25e033119f38bd091277a72f3bd1b
skelouse <sam@skelouse.com>
2020-11-02 12:12:36 -0600

    first commit

---
+++ test.py
@@ -0,0 +1,15 @@
+import numpy as np
+size = 12
+
+select = input("multiplication table size > ")
+try:
+    size = int(select)
+except ValueError:
+    print("Invalid Selection, Defaulting to 12")
+    size = 12
+
+final = np.zeros((size, size))
+for i in range(size):
+    for k in range(size):
+        final[i, k] = ((i+1) * (k+1))
+print(final.astype(int))
